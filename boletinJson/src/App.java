6a6aab100595207f100ce78064db1203afeda08c
manuel <manuelmat41@gmail.com>
2022-11-07 13:58:20 +0100

    Ejercicios hechos

--- boletinJson/src/App.java
+++ boletinJson/src/App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) {
         JsonManager jsonManager = new JsonManager();
-        jsonManager.readLocationWheatherData("pontevedra");
+        // jsonManager.readLocationWheatherData("pontevedra");
+        // jsonManager.readLocationWheatherData(-8.72264, 42.232819);
+        // jsonManager.readLocationWheatherData(-8.72264, 42.232819, 2);
+        // jsonManager.readLocationWheatherData(-8.72264, 42.232819);
+        // System.out.println(jsonManager.getLocationName(-8.72264, 42.232819));
+        // System.out.println(jsonManager.getCoordinatesFromLocation("pontevedra"));
+        System.out.println(jsonManager.getWheatherDataDate("dt", "vigo"));
     }
 }
--- boletinJson/src/JsonManager.java
+++ boletinJson/src/JsonManager.java
@@ -3,8 +3,12 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 import javax.json.Json;
+import javax.json.JsonObject;
 import javax.json.JsonValue;
 
 public class JsonManager {
@@ -35,6 +39,49 @@ public class JsonManager {
     }
 
     public void readLocationWheatherData(String location) {
-        System.out.println(readJson(String.format("http://api.openweathermap.org/data/2.5/weather?q=%s,es&lang=es&APPID=8f8dccaf02657071004202f05c1fdce0", location)).asJsonObject());
+        System.out.println((getLocationWheatherData(location)));
+    }
+
+    public JsonObject getLocationWheatherData(String location) {
+        return readJson(String.format("http://api.openweathermap.org/data/2.5/weather?q=%s,es&lang=es&APPID=8f8dccaf02657071004202f05c1fdce0", location)).asJsonObject();
+    }
+
+    public void readLocationWheatherData(double longitude, double latitude) {
+        System.out.println(readJson(String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&APPID=8f8dccaf02657071004202f05c1fdce0", latitude, longitude)).asJsonObject());
+    }
+
+    public void readLocationWheatherData(double longitude, double latitude, int countPredictions) {
+        System.out.println(readJson(String.format("http://api.openweathermap.org/data/2.5/find?lat=%f&lon=%f&cnt=%d&APPID=8f8dccaf02657071004202f05c1fdce0", latitude, longitude, countPredictions)).asJsonObject());
+    }
+    public JsonObject getLocationWheatherData(double longitude, double latitude) {
+        return readJson(String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&APPID=8f8dccaf02657071004202f05c1fdce0", latitude, longitude)).asJsonObject();
+    }
+
+    public int getLocationIdFromLocation(String location) {
+        return getLocationWheatherData(location).getInt("id");
+    }
+
+    public String getLocationNameFromCoordinates(double longitude, double latitude) {
+        return getLocationWheatherData(longitude, latitude).getString("name");
+    }
+
+    public String getCoordinatesFromLocation(String location) {
+        return String.format("Longitude: %f || Latitude: %f",getLocationWheatherData(location).getJsonObject("coord").getJsonNumber("lon").doubleValue(),getLocationWheatherData(location).getJsonObject("coord").getJsonNumber("lat").doubleValue());
+    }
+
+    public Object getWheatherDataInt(String dataName, String location) {
+        return getLocationWheatherData(location);
+    }
+    public Object getWheatherDataString(String dataName, String location) {
+        return getLocationWheatherData(location);
+    }
+    public Object getWheatherDataDouble(String dataName, String location) {
+        return getLocationWheatherData(location);
+    }
+    public Object getWheatherDataBoolean(String dataName, String location) {
+        return getLocationWheatherData(location);
+    }
+    public String getWheatherDataDate(String dataName, String location) {
+        return Instant.ofEpochSecond(getLocationWheatherData(location).getJsonNumber(dataName).longValue()).atZone(ZoneId.of("GMT+1")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 }
